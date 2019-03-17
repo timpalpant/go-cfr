1ea44a1b5570a54b5a234a7b8b0b2e44d0fbd0d4
Timothy Palpant <tim@palpant.us>
2019-03-17 11:19:24 -0400

    Implement gob encoding/decoding for thread-safe reservoir buffer

--- deepcfr/buffer.go
+++ deepcfr/buffer.go
@@ -1,6 +1,7 @@
 package deepcfr
 
 import (
+	"bytes"
 	"encoding/gob"
 	"math/rand"
 	"sync"
@@ -11,40 +12,80 @@ import (
 // samples are added via reservoir sampling, maintaining
 // a uniform distribution over all previous values.
 type ReservoirBuffer struct {
-	MaxSize int
-	Samples []Sample
-	N       int
+	maxSize int
+	samples []Sample
+	n       int
 }
 
 // NewBuffer returns an empty Buffer with the given max size.
 func NewReservoirBuffer(maxSize int) *ReservoirBuffer {
 	return &ReservoirBuffer{
-		MaxSize: maxSize,
+		maxSize: maxSize,
 	}
 }
 
 // AddSample implements Buffer.
 func (b *ReservoirBuffer) AddSample(s Sample) {
-	b.N++
+	b.n++
 
-	if len(b.Samples) < b.MaxSize {
-		b.Samples = append(b.Samples, s)
+	if len(b.samples) < b.maxSize {
+		b.samples = append(b.samples, s)
 	} else {
-		m := rand.Intn(b.N)
-		if m < b.MaxSize {
-			b.Samples[m] = s
+		m := rand.Intn(b.n)
+		if m < b.maxSize {
+			b.samples[m] = s
 		}
 	}
 }
 
 // GetSamples implements Buffer.
 func (b *ReservoirBuffer) GetSamples() []Sample {
-	return b.Samples
+	return b.samples
 }
 
 // Cap returns the max number of samples that will be kept in the buffer.
 func (b *ReservoirBuffer) Cap() int {
-	return b.MaxSize
+	return b.maxSize
+}
+
+// GobEncode implements gob.GobEncoder.
+func (b *ReservoirBuffer) GobEncode() ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+
+	if err := enc.Encode(b.maxSize); err != nil {
+		return nil, err
+	}
+
+	if err := enc.Encode(b.n); err != nil {
+		return nil, err
+	}
+
+	if err := enc.Encode(b.samples); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+// GobEncode implements gob.GobDecoder.
+func (b *ReservoirBuffer) GobDecode(buf []byte) error {
+	r := bytes.NewReader(buf)
+	dec := gob.NewDecoder(r)
+
+	if err := dec.Decode(&b.maxSize); err != nil {
+		return err
+	}
+
+	if err := dec.Decode(&b.n); err != nil {
+		return err
+	}
+
+	if err := dec.Decode(&b.samples); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // ThreadSafeReservoirBuffer wraps ReservoirBuffer to be safe for use
@@ -54,12 +95,14 @@ type ThreadSafeReservoirBuffer struct {
 	buf *ReservoirBuffer
 }
 
+// AddSample implements Buffer.
 func (b *ThreadSafeReservoirBuffer) AddSample(s Sample) {
 	b.mu.Lock()
 	b.buf.AddSample(s)
 	b.mu.Unlock()
 }
 
+// GetSamples implements Buffer.
 func (b *ThreadSafeReservoirBuffer) GetSamples() []Sample {
 	b.mu.Lock()
 	samples := b.buf.GetSamples()
@@ -67,6 +110,18 @@ func (b *ThreadSafeReservoirBuffer) GetSamples() []Sample {
 	return samples
 }
 
+// GobEncode implements gob.GobEncoder.
+func (b *ThreadSafeReservoirBuffer) GobEncode() ([]byte, error) {
+	return b.buf.GobEncode()
+}
+
+// GobEncode implements gob.GobDecoder.
+func (b *ThreadSafeReservoirBuffer) GobDecode(buf []byte) error {
+	return b.buf.GobDecode(buf)
+}
+
+// NewThreadSafeReservoirBuffer creates a new reservoir buffer with the
+// given max capacity that is safe for use from multiple goroutines.
 func NewThreadSafeReservoirBuffer(maxSize int) *ThreadSafeReservoirBuffer {
 	return &ThreadSafeReservoirBuffer{buf: NewReservoirBuffer(maxSize)}
 }
