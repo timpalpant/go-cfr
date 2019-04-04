d50b24ec7ebe99ffffb6d652d3bac958a89fd9ac
Timothy Palpant <tim@palpant.us>
2019-04-04 09:48:13 -0400

    Fix tests

--- kuhn/poker_test.go
+++ kuhn/poker_test.go
@@ -47,7 +47,7 @@ func TestPoker_ChanceSamplingCFR(t *testing.T) {
 
 func TestPoker_ExternalSamplingCFR(t *testing.T) {
 	policy := cfr.NewPolicyTable(cfr.DiscountParams{})
-	opt := cfr.NewExternalSampling(policy, cfr.NewSampledActionsMap)
+	opt := cfr.NewExternalSampling(policy)
 	testCFR(t, opt, policy, 200000)
 }
 
@@ -70,21 +70,21 @@ func TestPoker_AverageStrategySamplingCFR(t *testing.T) {
 
 func TestPoker_RobustSamplingCFR(t *testing.T) {
 	policy := cfr.NewPolicyTable(cfr.DiscountParams{})
-	opt := cfr.NewRobustSampling(policy, cfr.NewSampledActionsMap, 1)
+	opt := cfr.NewRobustSampling(policy, 1)
 	testCFR(t, opt, policy, 200000)
 }
 
 func TestPoker_CFRPlus(t *testing.T) {
 	plus := cfr.DiscountParams{UseRegretMatchingPlus: true}
 	policy := cfr.NewPolicyTable(plus)
-	opt := cfr.NewExternalSampling(policy, cfr.NewSampledActionsMap)
+	opt := cfr.NewExternalSampling(policy)
 	testCFR(t, opt, policy, 200000)
 }
 
 func TestPoker_LinearCFR(t *testing.T) {
 	linear := cfr.DiscountParams{LinearWeighting: true}
 	policy := cfr.NewPolicyTable(linear)
-	opt := cfr.NewExternalSampling(policy, cfr.NewSampledActionsMap)
+	opt := cfr.NewExternalSampling(policy)
 	testCFR(t, opt, policy, 200000)
 }
 
@@ -119,7 +119,7 @@ func BenchmarkPoker_ChanceSamplingCFR(b *testing.B) {
 
 func BenchmarkPoker_ExternalSamplingCFR(b *testing.B) {
 	policy := cfr.NewPolicyTable(cfr.DiscountParams{})
-	opt := cfr.NewExternalSampling(policy, cfr.NewSampledActionsMap)
+	opt := cfr.NewExternalSampling(policy)
 	b.ResetTimer()
 	runCFR(b, opt, policy, b.N)
 }
@@ -206,11 +206,11 @@ func (m randomGuessModel) Predict(infoSet cfr.InfoSet, nActions int) []float32 {
 func TestPoker_DeepCFR(t *testing.T) {
 	model := &randomGuessModel{}
 	gob.Register(model)
-	buf0 := deepcfr.NewReservoirBuffer(10)
-	buf1 := deepcfr.NewReservoirBuffer(10)
-	deepCFR := deepcfr.New(model, []deepcfr.Buffer{buf0, buf1})
+	buf0 := deepcfr.NewReservoirBuffer(10, 1)
+	buf1 := deepcfr.NewReservoirBuffer(10, 1)
+	deepCFR := deepcfr.New(model, []deepcfr.Buffer{buf0, buf1}, true)
 	root := NewGame()
-	opt := cfr.NewExternalSampling(deepCFR, cfr.NewSampledActionsMap)
+	opt := cfr.NewExternalSampling(deepCFR)
 	for i := 1; i <= 1000; i++ {
 		opt.Run(root)
 	}
