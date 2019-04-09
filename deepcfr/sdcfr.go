2d6ebe8d755c3501285bb1e49d5dd1a76e7c91e6
Timothy Palpant <tim@palpant.us>
2019-04-08 22:12:00 -0400

    Implement other StrategyProfile methods as stubs on TrajectorySampledSDCFR

--- deepcfr/sdcfr.go
+++ deepcfr/sdcfr.go
@@ -3,10 +3,22 @@ package deepcfr
 import (
 	"math/rand"
 
+	"github.com/golang/glog"
+
 	"github.com/timpalpant/go-cfr"
 )
 
 type TrajectorySampledSDCFR []TrainedModel
+
+func (d TrajectorySampledSDCFR) Close() error {
+	return nil
+}
+
+func (d TrajectorySampledSDCFR) Iter() int {
+	return -1
+}
+
+func (d TrajectorySampledSDCFR) Update() {}
 
 func (d TrajectorySampledSDCFR) GetPolicy(node cfr.GameTreeNode) cfr.NodePolicy {
 	return &sampledDCFRPolicy{
@@ -53,12 +65,13 @@ func sampleModels(models [][]TrainedModel) []TrainedModel {
 }
 
 func sampleModel(candidates []TrainedModel) TrainedModel {
-	m := len(candidates) * (len(candidates) - 1) / 2
+	m := len(candidates) * (len(candidates) + 1) / 2
 	x := rand.Intn(m) + 1
 	n := 0
 	for i := range candidates {
 		n += i + 1
 		if n >= x {
+			glog.V(3).Infof("Sampled model %d (out of %d)", i, len(candidates))
 			return candidates[i]
 		}
 	}
