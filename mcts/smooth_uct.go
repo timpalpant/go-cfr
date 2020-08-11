8faa97d09c4cd973d1e478c36b34069c6942a18d
Timothy Palpant <tim@palpant.us>
2020-08-11 01:32:51 +0000

    Make MCTS thread-safe

--- mcts/smooth_uct.go
+++ mcts/smooth_uct.go
@@ -3,57 +3,46 @@ package mcts
 import (
 	"math"
 	"math/rand"
+	"sync"
 
 	"github.com/timpalpant/go-cfr"
 	"github.com/timpalpant/go-cfr/sampling"
 )
 
 type mctsNode struct {
+	mx           sync.Mutex
 	visits       []int
 	totalRewards []float32
 }
 
-func newMCTSNode(n int) mctsNode {
-	return mctsNode{
+func newMCTSNode(n int) *mctsNode {
+	return &mctsNode{
 		visits:       make([]int, n),
 		totalRewards: make([]float32, n),
 	}
 }
 
-func (s mctsNode) totalVisits() int {
-	n := 0
-	for _, v := range s.visits {
-		n += v
-	}
-	return n
-}
-
-func (s mctsNode) value() float32 {
-	var result float32
-	for i, n := range s.visits {
-		result += s.totalRewards[i] / float32(n)
-	}
-
-	return result
-}
-
-func (s mctsNode) rewards() []float32 {
-	result := make([]float32, len(s.totalRewards))
-	for i, n := range s.visits {
-		result[i] = s.totalRewards[i] / float32(n)
-	}
-
-	return result
-}
-
-func (s mctsNode) update(action int, reward float32) {
+func (s *mctsNode) update(action int, reward float32) {
+	s.mx.Lock()
+	defer s.mx.Unlock()
 	s.visits[action] = s.visits[action] + 1
 	s.totalRewards[action] += reward
 }
 
 // Follows the notation of Heinrich and Silver (2015).
-func (s mctsNode) selectAction(c, gamma, eta, d float32) int {
-	etaK := eta / (1.0 + d*float32(math.Sqrt(float64(s.totalVisits()))))
+func (s *mctsNode) selectAction(c, gamma, eta, d float32) int {
+	s.mx.Lock()
+	defer s.mx.Unlock()
+	totalVisits := 0
+	for i, v := range s.visits {
+		if v == 0 {
+			return i // Pull unpulled arm.
+		}
+
+		totalVisits += v
+	}
+
+	etaK := eta / (1.0 + d*float32(math.Sqrt(float64(totalVisits))))
 	if etaK < gamma {
 		etaK = gamma
 	}
@@ -65,12 +54,8 @@ func (s mctsNode) selectAction(c, gamma, eta, d float32) int {
 		qs := make([]float32, len(s.visits))
 		vs := make([]float32, len(s.visits))
 		for i, n := range s.visits {
-			if n == 0 {
-				return i // Pull unpulled arm.
-			}
-
 			q := s.totalRewards[i] / float32(n)
-			v := q + c*float32(math.Sqrt(math.Log(float64(s.totalVisits()))/float64(n)))
+			v := q + c*float32(math.Sqrt(math.Log(float64(totalVisits))/float64(n)))
 			if v > vMax {
 				selected = i
 				vMax = v
@@ -87,26 +72,33 @@ func (s mctsNode) selectAction(c, gamma, eta, d float32) int {
 	}
 
 	p := stackalloc(len(s.visits))
-	s.fillAverageStrategy(p)
+	s.fillAverageStrategyUnsafe(p, 1.0)
 	selected := sampling.SampleOne(p, rand.Float32())
 	return selected
 }
 
-func (s mctsNode) averageStrategy() []float32 {
+func (s *mctsNode) averageStrategy(temperature float32) []float32 {
+	s.mx.Lock()
+	defer s.mx.Unlock()
 	p := make([]float32, len(s.visits))
-	s.fillAverageStrategy(p)
+	s.fillAverageStrategyUnsafe(p, temperature)
 	return p
 }
 
-func (s mctsNode) fillAverageStrategy(p []float32) {
-	nTotal := float32(s.totalVisits())
+func (s *mctsNode) fillAverageStrategyUnsafe(p []float32, temperature float32) {
+	var nTotal float64
+	for _, n := range s.visits {
+		nTotal += math.Pow(float64(n), 1.0/float64(temperature))
+	}
+
 	if nTotal == 0 {
 		for i := range p {
 			p[i] = 1.0 / float32(len(p))
 		}
 	} else {
 		for i, n := range s.visits {
-			p[i] = float32(n) / nTotal
+			x := math.Pow(float64(n), 1.0/float64(temperature))
+			p[i] = float32(x / nTotal)
 		}
 	}
 }
@@ -124,44 +116,37 @@ func stackalloc(n int) []float32 {
 
 // Implements Smooth UCT.
 type SmoothUCT struct {
-	iterations int
-	c          float32
-	gamma      float32
-	eta        float32
-	d          float32
+	c     float32
+	gamma float32
+	eta   float32
+	d     float32
 
-	tree        map[string]mctsNode
-	isOutOfTree [2]bool
+	mx   sync.Mutex
+	tree map[string]*mctsNode
 }
 
-func NewSmoothUCT(iterations int, c, gamma, eta, d float32) *SmoothUCT {
+func NewSmoothUCT(c, gamma, eta, d float32) *SmoothUCT {
 	return &SmoothUCT{
-		iterations: iterations,
-		c:          c,
-		gamma:      gamma,
-		eta:        eta,
-		d:          d,
+		c:     c,
+		gamma: gamma,
+		eta:   eta,
+		d:     d,
 
-		tree: make(map[string]mctsNode),
+		tree: make(map[string]*mctsNode),
 	}
 }
 
 func (s *SmoothUCT) Run(node cfr.GameTreeNode) float32 {
-	rootPlayer := node.Player()
-
-	var ev float32
-	for i := 0; i < s.iterations; i++ {
-		ev += s.simulate(node, rootPlayer) / float32(s.iterations)
-	}
-
-	return ev
+	return s.simulate(node, node.Player(), [2]bool{false, false})
 }
 
-func (s *SmoothUCT) GetPolicy(node cfr.GameTreeNode) []float32 {
+func (s *SmoothUCT) GetPolicy(node cfr.GameTreeNode, temperature float32) []float32 {
+	s.mx.Lock()
+	defer s.mx.Unlock()
 	u := node.InfoSet(node.Player()).Key()
 	treeNode, ok := s.tree[u]
 	if ok {
-		return treeNode.averageStrategy()
+		return treeNode.averageStrategy(temperature)
 	}
 
 	return uniform(node.NumChildren())
@@ -175,17 +160,17 @@ func uniform(n int) []float32 {
 	return result
 }
 
-func (s *SmoothUCT) simulate(node cfr.GameTreeNode, lastPlayer int) float32 {
+func (s *SmoothUCT) simulate(node cfr.GameTreeNode, lastPlayer int, isOutOfTree [2]bool) float32 {
 	var ev float32
 	switch node.Type() {
 	case cfr.TerminalNodeType:
 		ev = float32(node.Utility(lastPlayer))
 	case cfr.ChanceNodeType:
 		child, _ := node.SampleChild()
-		ev = s.simulate(child, lastPlayer)
+		ev = s.simulate(child, lastPlayer, isOutOfTree)
 	default:
 		sgn := getSign(lastPlayer, node.Player())
-		ev = sgn * s.handlePlayerNode(node)
+		ev = sgn * s.handlePlayerNode(node, isOutOfTree)
 	}
 
 	node.Close()
@@ -200,35 +185,32 @@ func getSign(player1, player2 int) float32 {
 	return -1.0
 }
 
-func (s *SmoothUCT) handlePlayerNode(node cfr.GameTreeNode) float32 {
+func (s *SmoothUCT) handlePlayerNode(node cfr.GameTreeNode, isOutOfTree [2]bool) float32 {
 	i := node.Player()
-	if s.isOutOfTree[i] {
-		return s.rollout(node)
+	if isOutOfTree[i] {
+		return s.rollout(node, isOutOfTree)
 	}
 
 	u := node.InfoSet(i).Key()
+	s.mx.Lock()
 	treeNode, ok := s.tree[u]
-	var action int
 	if !ok { // Expand tree.
 		numChildren := node.NumChildren()
 		treeNode = newMCTSNode(numChildren)
 		s.tree[u] = treeNode
-		action = rand.Intn(numChildren)
-		s.isOutOfTree[i] = true
-		defer func() { s.isOutOfTree[i] = false }()
-	} else {
-		action = treeNode.selectAction(s.c, s.gamma, s.eta, s.d)
+		isOutOfTree[i] = true
 	}
+	s.mx.Unlock()
 
+	action := treeNode.selectAction(s.c, s.gamma, s.eta, s.d)
 	child := node.GetChild(action)
-	reward := s.simulate(child, i)
+	reward := s.simulate(child, i, isOutOfTree)
 	treeNode.update(action, reward)
 	return reward
 }
 
-func (s *SmoothUCT) rollout(node cfr.GameTreeNode) float32 {
+func (s *SmoothUCT) rollout(node cfr.GameTreeNode, isOutOfTree [2]bool) float32 {
 	action := rand.Intn(node.NumChildren())
 	child := node.GetChild(action)
-	result := s.simulate(child, node.Player())
-	return result
+	return s.simulate(child, node.Player(), isOutOfTree)
 }
